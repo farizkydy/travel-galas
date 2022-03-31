86e004bcd447087113285ed8296e88520a487935
farizkydy <farizrizkyrizaldy@gmail.com>
2022-03-31 09:53:00 +0700

    Initial Commit

---
+++ galas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  galas
+//
+//  Created by Fariz Rizky Rizaldy on 31/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
