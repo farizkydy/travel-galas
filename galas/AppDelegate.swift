09a448f15019bcdf015954eeafbf34c73ce53ee0
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2022-03-31 14:49:36 +0700

    Change AppDelegate line 25 to Default Configuration

--- galas/AppDelegate.swift
+++ galas/AppDelegate.swift
@@ -3,7 +3,6 @@
 //  galas
 //
 //  Created by Fariz Rizky Rizaldy on 31/03/22.
-// hai
 
 import UIKit
 
@@ -22,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Testttiiii", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
