5331b28e14c12c29858b8a0443cf0c06ae5b6621
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2022-04-04 17:15:37 +0700

    Create Select Activities Page

---
+++ galas/Select Activities/SelectActivitiesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SelectActivitiesViewController.swift
+//  galas
+//
+//  Created by Renzo Alvaroshan on 04/04/22.
+//
+
+import UIKit
+
+class SelectActivitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
