f9f6c7276ca23be2c77083dda08208c3f108c1f1
xxshimatta <jeffreyclay00@gmail.com>
2022-04-04 21:32:26 +0700

    new files

---
+++ galas/DashboardController.swift
@@ -0,0 +1,29 @@
+//
+//  DashboardController.swift
+//  galas
+//
+//  Created by Jeffrey Clay Setiawan on 04/04/22.
+//
+
+import UIKit
+
+class DashboardController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
